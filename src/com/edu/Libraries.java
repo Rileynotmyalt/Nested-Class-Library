cb28aa03b3d9cdd82744cf0869cd8f6847e18d96
Riley <rcarter41@claytonschools.net>
2022-01-24 09:04:47 -0600

    ooooo Constructors

--- src/com/edu/Libraries.java
+++ src/com/edu/Libraries.java
@@ -3,7 +3,7 @@ package com.edu;
 public class Libraries {
     public static class Missouri {
         public static class StLouis {
-            { // constructor
+            static { // constructor
                 //I can do some cool stuff with this. maybe get all classes from here for UI?
                 System.out.println("This is run on init");
             }
